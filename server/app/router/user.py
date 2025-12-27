74799b05305d71ad007b3d00154428df0af9a696
SUJOY GARAI <sujoycode999@gmail.com>
2025-12-27 15:52:18 +0530

    add user router functions for retrieving user by ID, email, and login

--- server/app/router/user.py
+++ server/app/router/user.py
@@ -1,2 +1,11 @@
 from ..model import user
 
+
+def get_user_by_id(user_id):
+    return user.get_user_by_id(user_id)
+
+def get_user_by_email(email):
+    return user.get_user_by_email(email)
+
+def User_login(email, password):
+    return user.User_login(email, password) 
