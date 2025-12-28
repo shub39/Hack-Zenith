2302b8b65b4888127a85a83e0d944c8310bed43e
SUJOY GARAI <sujoycode999@gmail.com>
2025-12-28 21:49:06 +0530

    add MongoDB connection setup and database retrieval function

---
+++ server/app/db/mongodb.py
@@ -0,0 +1,8 @@
+from motor.motor_asyncio import AsyncIOMotorClient
+from app.core.config import MONGODB_URL, DATABASE_NAME
+
+client = AsyncIOMotorClient(MONGODB_URL)
+database = client[DATABASE_NAME]
+
+def get_database():
+    return database
