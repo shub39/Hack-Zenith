876c01ae59af36ecf286c43f87307a870ee99856
SUJOY GARAI <sujoycode999@gmail.com>
2025-12-29 11:31:23 +0530

    fix import path for MongoDB configuration and add config file

--- server/app/db/mongodb.py
+++ server/app/db/mongodb.py
@@ -1,5 +1,5 @@
 from motor.motor_asyncio import AsyncIOMotorClient
-from app.core.config import MONGODB_URL, DATABASE_NAME
+from config import MONGODB_URL, DATABASE_NAME
 
 client = AsyncIOMotorClient(MONGODB_URL)
 database = client[DATABASE_NAME]
